e2b650c8ad1989dc9f24ce747220ee8be0a2fe5a
wisnuciwun <adiwardhanawisnu@gmail.com>
2021-10-03 15:37:19 +0700

    learn part 1 - framework installation, app.vue intro, basic function

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+<p class="text" >{{ title }}</p>
+<input type="text" ref="testref">
+<button v-on:click="handleClick">Go find it !</button>
+<Modal />
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: { Modal },
+  name: 'App',
+  data () {
+    return {title: 'Seaaarch'}
+  },
+  methods: {
+    handleClick(){
+      this.$refs.testref.classList.add('active')
+      this.$refs.testref.focus()
+    }
+  }
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.text{
+  background-color: burlywood;
+  font-size: 30pt;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="shadows" >
+        <div class="modal-header">Sorry :(</div>
+        <div class="modal-body">
+            <p> We still on development, wait us until everything is wrapped up at October 2K21</p>
+        </div>
+    </div>
+</template>
+
+<style>
+.shadows{
+    background: black;
+    opacity: 50%;
+    border-radius: 10px;
+    top: 0;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+}
+
+.modal-body {
+    width: 500px;
+    height: 300px;
+    margin: 100px auto;
+    background: coral;
+    border-radius: 10px;
+    color: white;
+}
+
+.modal-header{
+    font-size: 30pt;
+    color: black;
+}
+
+</style>
